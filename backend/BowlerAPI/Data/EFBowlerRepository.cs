8ab060c928f684c7f688af97ca09122f958646f5
calvertem <calvertemma25@gmail.com>
2024-03-15 12:52:12 -0600

    All Changes

---
+++ backend/BowlerAPI/Data/EFBowlerRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace BowlerAPI.Data
+{
+	public class EFBowlerRepository : IBowlerRepository
+	{
+		private BowlerContext _context;
+		public EFBowlerRepository(BowlerContext temp) {
+			_context = temp;
+		}
+		public IEnumerable<Bowler> Bowlers => _context.Bowlers.ToList();
+
+		public IEnumerable<Team> Teams => _context.Teams.ToList();
+
+
+	}
+}
---
+++ backend/BowlerAPI/Data/IBowlerRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace BowlerAPI.Data
+{
+	public interface IBowlerRepository
+	{
+		IEnumerable<Bowler> Bowlers { get; }
+
+		IEnumerable<Team> Teams { get; }
+	}
+}
